b77838ce3bcb0910db61ede584a4e2dcbf38f2c4
Ishan Galhotra <ishaangalhotra@gmail.com>
2025-06-13 16:14:26 +0530

    Fix: cleaned up product router to prevent module error

--- routes/products.js
+++ routes/products.js
@@ -1,2 +1,9 @@
-const productRoutes = require('./routes/products');
-app.use('/api/products', productRoutes);
+const express = require('express');
+const router = express.Router();
+
+// Sample product route
+router.get('/', (req, res) => {
+  res.send('Product route working!');
+});
+
+module.exports = router;
