3947a7c74162f62996fe3f648477d643fb76884b
Ishan Galhotra <ishaangalhotra@gmail.com>
2025-07-02 13:02:37 +0530

    Fix: Corrected middleware path to 'middlewares/authMiddleware'

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware');
 const { createProduct, getProducts } = require('../controllers/productController');
 
 // GET all products
