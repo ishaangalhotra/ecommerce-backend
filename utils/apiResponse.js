49c90d8a6c162bb14a98f510f6336ad4757d64ae
Ishan Galhotra <ishaangalhotra@gmail.com>
2025-09-19 16:29:58 +0530

    Fix apiResponse utility (syntax error and helpers)

--- utils/apiResponse.js
+++ utils/apiResponse.js
@@ -1,37 +1,58 @@
- DbackendutilsapiResponse.js
+// utils/apiResponse.js
+// Small helper for consistent API responses
 
 class ApiResponse {
-    constructor(statusCode, data, message = Success) {
-        this.statusCode = statusCode;
-        this.data = data;
-        this.message = message;
-        this.success = statusCode  400;
-    }
+  constructor(statusCode = 400, success = false, message = '', data = null) {
+    this.statusCode = statusCode;
+    this.success = success;
+    this.message = message;
+    this.data = data;
+  }
+
+  toJSON() {
+    return {
+      success: this.success,
+      message: this.message,
+      data: this.data
+    };
+  }
+
+  send(res) {
+    return res.status(this.statusCode).json(this.toJSON());
+  }
+
+  // Convenience static helpers
+  static ok(res, data = null, message = 'OK', statusCode = 200) {
+    return res.status(statusCode).json({
+      success: true,
+      message,
+      data
+    });
+  }
+
+  static created(res, data = null, message = 'Created') {
+    return this.ok(res, data, message, 201);
+  }
+
+  static error(res, message = 'Internal Server Error', statusCode = 500, data = null) {
+    return res.status(statusCode).json({
+      success: false,
+      message,
+      data
+    });
+  }
+
+  static badRequest(res, message = 'Bad Request', data = null) {
+    return this.error(res, message, 400, data);
+  }
+
+  static unauthorized(res, message = 'Unauthorized', data = null) {
+    return this.error(res, message, 401, data);
+  }
+
+  static notFound(res, message = 'Not Found', data = null) {
+    return this.error(res, message, 404, data);
+  }
 }
 
-class ApiError extends Error {
-    constructor(
-        statusCode,
-        message = Something went wrong,
-        errors = [],
-        stack = 
-    ) {
-        super(message);
-        this.statusCode = statusCode;
-        this.data = null;
-        this.message = message;
-        this.success = false;
-        this.errors = errors;
-
-        if (stack) {
-            this.stack = stack;
-        } else {
-            Error.captureStackTrace(this, this.constructor);
-        }
-    }
-}
-
-module.exports = {
-    ApiResponse,
-    ApiError
-};
+module.exports = ApiResponse;
